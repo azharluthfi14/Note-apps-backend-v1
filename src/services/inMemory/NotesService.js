a09851eeb470f6ac9f712e2ae3ae9cd6dbcaefbc
azharluthfi14 <35468624+azharluthfi14@users.noreply.github.com>
2022-04-10 22:17:50 +0700

    Create handler,routes,services note

---
+++ src/services/inMemory/NotesService.js
@@ -0,0 +1,79 @@
+// Handle resource Note before save in memory (Array)
+
+const { nanoid } = require("nanoid");
+
+class NotesService {
+  constructor() {
+    this._notes = [];
+  }
+
+  // Crud Method
+  addNote({ title, body, tags }) {
+    const id = nanoid(16);
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+
+    const newNote = {
+      id,
+      title,
+      tags,
+      body,
+      createdAt,
+      updatedAt,
+    };
+
+    this._notes.push(newNote);
+
+    const isSuccess = this._notes.filter((note) => note.id === id).length > 0;
+
+    // If not success, show error
+    if (!isSuccess) {
+      throw new Error("Catatan gagal ditambahkan");
+    }
+
+    return id;
+  }
+
+  getNotes() {
+    return this._notes;
+  }
+
+  getNotesById(id) {
+    const note = this._notes.filter((n) => n.id === id)[0];
+
+    if (!note) {
+      throw new Error("Catatan tidak ditemukan");
+    }
+    return note;
+  }
+
+  editNoteById(id, { title, body, tags }) {
+    const index = this._notes.findIndex((note) => note.id === id);
+
+    if (index === -1) {
+      throw new Error("Gagal memperbarui catatan. Id tidak ditemukan");
+    }
+
+    const updatedAt = new Date().toISOString();
+
+    this._notes[index] = {
+      ...this._notes[index],
+      title,
+      tags,
+      body,
+      updatedAt,
+    };
+  }
+
+  deleteNoteById(id) {
+    const index = this._notes.findIndex((note) => note.id === id);
+
+    if (index === -1) {
+      throw new Error("Catatan gagal dihapus. Id tidak ditemukan");
+    }
+
+    this._notes.splice(index, 1);
+  }
+}
+
+module.exports = NotesService;
